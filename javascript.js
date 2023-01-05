0ece982828f62c1709b69dc3d7ecb296199b86b4
Saad <saadjamshaid000@gmail.com>
2023-01-06 03:17:29 +0500

    Implemented grid size selection buttons for 8x8, 16x16, and 32x32 grids.
    Function is implemented according to these 3 buttons and grid size boxes will appear w.r.t which button is selected

--- javascript.js
+++ javascript.js
@@ -12,19 +12,32 @@ const button = document.createElement('button');
     button.className = "button1";
     control.appendChild(button);
     button.innerText = "8×8";
+    button.addEventListener('click', onclicking1);
 
 const button2 = document.createElement('button');
-    button.className = "button2"
+    button2.className = "button2";
     control.appendChild(button2);
-    button2.innerText = "16×16";    
+    button2.innerText = "16×16";
+    button2.addEventListener('click', onclicking2);
+    button2.addEventListener('click', toggleBiggerClass);
+    function toggleBiggerClass() {
+    const onbutton2 = document.querySelector('.squarebox');
+    onbutton2.classList.toggle('bigger16by16');
+}
 
 const button3 = document.createElement('button');
-    button.className = "button3"
+    button3.className = "button3";
     control.appendChild(button3);
     button3.innerText = "32×32";
+    button3.addEventListener('click', onclicking3);
+    button3.addEventListener('click', toggleBiggerClass2);
+    function toggleBiggerClass2() {
+    const onbutton3 = document.querySelector('.squarebox');
+    onbutton3.classList.toggle('bigger32by32');
+    }
 
 const button4 = document.createElement('button');
-    button.className = "button4"
+    button4.className = "button4";
     control.appendChild(button4);
     button4.innerText = "64×64";        
 
@@ -32,12 +45,43 @@ const squarediv = document.createElement('div');
     squarediv.className = "squarebox";
     container.appendChild(squarediv);    
 
-
-for (let i=0; i<256; i++) {
-    const smalldiv = document.createElement('div');
-    smalldiv.className = "smalldiv";
-    squarediv.appendChild(smalldiv);
-    smalldiv.addEventListener('mouseover', function() {
+function onclicking1() {
+    for (let i=0; i<64; i++) {
+        const smalldiv = document.createElement('div');
+        smalldiv.className = "smalldiv";
+        squarediv.appendChild(smalldiv);
+        smalldiv.addEventListener('mouseover', function() {
         smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
     });
+}   }
+
+function onclicking2() {
+    for(i=0; i<256; i++) {
+        const smalldiv = document.createElement('div');
+        smalldiv.className = "smalldiv";
+        squarediv.appendChild(smalldiv);
+        smalldiv.addEventListener('mouseover', function() {
+        smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
+    });
+}    }  
+
+function onclicking3() {
+    for(i=0; i<1024; i++) {
+    const smalldiv = document.createElement('div');
+    smalldiv.className = "smalldiv";          
+    squarediv.appendChild(smalldiv);
+    smalldiv.addEventListener('mouseover', function() {
+    smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
+    });
 }
+}
+
+
+
+// for (let i=0; i<256; i++) {
+//     const smalldiv = document.createElement('div');
+//     smalldiv.className = "smalldiv";
+//     squarediv.appendChild(smalldiv);
+//     smalldiv.addEventListener('mouseover', function() {
+//         smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
+//     });
