d6bb0e0019e8a72cda34a69b97fcc04417c9872c
Saad <saadjamshaid000@gmail.com>
2023-01-06 02:25:59 +0500

    Add buttons for creating 8x8, 16x16, 32x32, and 64x64 grids

--- javascript.js
+++ javascript.js
@@ -3,15 +3,41 @@ const container = document.createElement('div');
     container.className = "container";
     body.appendChild(container);
 
+const control = document.createElement('div');
+    control.className = "control";
+    container.appendChild(control);
+    control.innerText = "Select the Grid Size";
+
+const button = document.createElement('button');
+    button.className = "button1";
+    control.appendChild(button);
+    button.innerText = "8×8";
+
+const button2 = document.createElement('button');
+    button.className = "button2"
+    control.appendChild(button2);
+    button2.innerText = "16×16";    
+
+const button3 = document.createElement('button');
+    button.className = "button3"
+    control.appendChild(button3);
+    button3.innerText = "32×32";
+
+const button4 = document.createElement('button');
+    button.className = "button4"
+    control.appendChild(button4);
+    button4.innerText = "64×64";        
+
 const squarediv = document.createElement('div');
     squarediv.className = "squarebox";
-    container.appendChild(squarediv);
+    container.appendChild(squarediv);    
+
 
 for (let i=0; i<256; i++) {
     const smalldiv = document.createElement('div');
     smalldiv.className = "smalldiv";
     squarediv.appendChild(smalldiv);
     smalldiv.addEventListener('mouseover', function() {
-    smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
+        smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
     });
 }
