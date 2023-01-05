cb811d0b773800f0a9b478aab324477f29910a4d
Saad <saadjamshaid000@gmail.com>
2023-01-05 23:56:04 +0500

    Created 16x16 grid of small div elements inside squarebox element

--- javascript.js
+++ javascript.js
@@ -1,8 +1,17 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
-container.className = "container";
-body.appendChild(container);
+    container.className = "container";
+    body.appendChild(container);
 
 const squarediv = document.createElement('div');
-squarediv.className = "squarebox";
-container.appendChild(squarediv);
+    squarediv.className = "squarebox";
+    container.appendChild(squarediv);
+
+for (let i=0; i<256; i++) {
+    const smalldiv = document.createElement('div');
+    smalldiv.className = "smalldiv";
+    squarediv.appendChild(smalldiv);
+    smalldiv.addEventListener('mouseover', function() {
+    smalldiv.style.backgroundColor = 'rgba(0, 0, 0, 0.92)';
+    });
+}
