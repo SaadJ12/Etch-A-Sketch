67be952c012e12e625d1a18c603081c97ac71790
Saad <saadjamshaid000@gmail.com>
2023-01-05 20:38:27 +0500

    Add squarebox divs and container div

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.className = "container";
+body.appendChild(container);
+
+const squarediv = document.createElement('div');
+squarediv.className = "squarebox";
+container.appendChild(squarediv);
